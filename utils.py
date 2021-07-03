565c1b1e23ec30c64eb5f5eea67c04e6f3311ed5
mutenpo <msi7va@gmail.com>
2021-07-03 14:34:43 -0500

    Update utils.py
    Removed static attribute keys and values and switched to dynamic assignment.

--- utils.py
+++ utils.py
@@ -3,41 +3,23 @@ import os
 
 
 class Credentials:
-    def __init__(self, username=None, password=None, token=None, url=None):
-        self.username = username
-        self.password = password
-        self.token = token
-        self.url = url
+    def __init__(self, argv):
+        for key, val in argv.items():
+            self.__dict__[key] = val
+
 
     def __repr__(self):
-        return "{}".format(self.username, self.password, self.token, self.url)
+        return f"These are the available keys: {list(self.__dict__.keys()}."
 
-    def json(self):
-        return {
-            'username': self.username,
-            'password': self.password,
-            'token': self.token,
-            'url': self.url
-        }
 
     @classmethod
     def from_json(cls, json_data, filepath=None):
         if filepath:
             header_data = os.path.join(filepath, json_data)
             with open(header_data, 'r') as f:
-                headers = json.load(f)
-                for header in headers:
-                    url = headers['url']
-                    username = headers['username']
-                    password = headers['password']
-                    token = headers['token']
-                return Credentials(**headers)
+                key_dict = json.load(f)
+                return Credentials(key_dict)
         else:
             with open(json_data, 'r') as f:
-                headers = json.load(f)
-                for header in headers:
-                    url = headers['url']
-                    username = headers['username']
-                    password = headers['password']
-                    token = headers['token']
-                return Credentials(**headers)
+                key_dict = json.load(f)
+                return Credentials(key_dict)
